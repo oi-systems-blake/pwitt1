a99e4b56b63430062dd1454a9e443952e5442cf6
oi-systems-blake <blake@oi-systems.com>
2022-11-02 19:25:07 -0500

    date for ts but not css for ts

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import TestClock from "./routing/TestClock"
 import TimeTest from './routing/TimeTest';
 import { TestComp } from "./pages/ClockInPage/TestComp"
 import awsExports from './aws-exports';
+import { TsTimeSheetGrab } from './pages/ViewTimesheet/TsTimeSheetGrab';
 Amplify.configure(awsExports);
 
 
--- src/pages/ViewTimesheet/ViewTimesheet.js
+++ src/pages/ViewTimesheet/ViewTimesheet.js
@@ -22,7 +22,9 @@ export function ViewTimesheet() {
   let actualStartOfWeek = parseFloat(startOfWeekAsString, 10);
 
   useEffect(() => {
-    setTime(new Date())
+    setTime(new Date());
+    TsTimeSheetGrab();
+    console.log(actualStartOfWeek, actualDateNumber);
   }, []);
 
   // const { authStatus } = useAuthenticator((context) => [context.user]);
@@ -41,16 +43,13 @@ export function ViewTimesheet() {
         },
       })
     );
-    tsGrabber.then((timeSheets) => {
-      //  timeSheet = timeSheets.data.listTimeSheets.items
-
-      let timeSheet = timeSheets.data.listTimeSheets.items;
-      console.log(timeSheet);
-        setTimeSheets(timeSheet)
-      console.log(typeof timeSheet);
+    tsGrabber.then((x) => {
+      console.log(x);
+      let y = x.data.listTimeSheets.items;
+      setTimeSheets(y);
     });
   }
- // TsTimeSheetGrab();
+  // TsTimeSheetGrab();
 
   // console.log(timeSheets)
   // const TimeSheet = [
@@ -84,60 +83,34 @@ export function ViewTimesheet() {
 
   // console.log(authStatus);
   return (
-    <>
-      <div className="view-timesheet-page">
-        <div className="view-timesheet-sidebar"></div>
-        <button onClick={TsTimeSheetGrab}></button>
-        <div>
-        {Object.keys(timeSheets).map((value, index) => {
-          <div key={index} className="timesheet-row">
-          <div className="timesheet-date">
-            {value.dayName}, {value.month} {value.dayNumber}
-          </div>
-{/*           <div className="timesheet-hour">
-            <div className="timesheet-hour-text">
-              8h 00m{" "}
-              {value.punches.map((hour) => (
-                <div className="individual-hour">
-                  {hour[0] + " - " + hour[1]}
+    <div className="view-timesheet-page">
+      <div className="view-timesheet-sidebar"></div>
+      <div className="timesheet-container">
+        <div className="timesheet-header timesheet-month">May</div>
+        <div className="timesheet-header timesheet-days">1st - 6th</div>
+        {Object.values(timeSheets).map((value, index) => {
+          return (
+            <div key={index} className="timesheet-row">
+              <div className="timesheet-date">
+                {value.dayName}, {value.month} {value.dayNumber}
+              </div>
+              <div className="timesheet-hour">
+                <div className="timesheet-hour-text">
+                  8h 00m
+                  <div className="individual-hour">
+                    {value.punches.map((hour, index) => (
+                      <div key={index} className="ts-individual-punch">
+                        {hour}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              ))}
+              </div>
             </div>
-          </div> */}
-        </div>
-            // <div key={index}>
-            //   <h2>
-            //     {key}: {timeSheets[key]}
-            //   </h2>
-  
-            //   <hr />
-            // </div>
-        })}</div>
-        {/* <TsTimeSheetGrab pin={pin} actualStartOfWeek={actualStartOfWeek} actualDateNumber={actualDateNumber} /> */}
-        {/*    <div className="timesheet-container">
-      <div className="timesheet-header timesheet-month">May</div>
-      <div className="timesheet-header timesheet-days">1st - 6th</div>
-      {timeSheets.map((day) => (
-        <div className="timesheet-row">
-          <div className="timesheet-date">
-            {day.dayName}, {day.month} {day.dayNumber}
-          </div>
-          <div className="timesheet-hour">
-            <div className="timesheet-hour-text">
-              8h 00m{" "}
-              {day.hours.map((hour) => (
-                <div className="individual-hour">
-                  {hour[0] + " - " + hour[1]}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      ))}
-
-      <div className="timesheet-hour-total"> 48h00m</div>
-    </div>  */}
+          );
+        })}
+        <div className="timesheet-hour-total"> 48h00m</div>
       </div>
-    </>
+    </div>
   );
 }
